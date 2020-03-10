9353b7a3cb2a1e514d840be247299cfb8a11dbca
[Vini] <[vantoninicad@protonmail.com]>
2020-03-09 22:58:17 -0400

    Add project files.

---
+++ HomeControlDAL/HomeControlEntitty.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeControlDAL {
+    public class HomeControlEntitty {
+
+        public int Id { get; set; }
+        public DateTime DateModified { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ HomeControlDAL/Model/Categories.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HomeControlDAL {
+    public class Categories : HomeControlEntitty {
+        public Categories() {
+            //Employees = new HashSet<Employees>();
+        }
+
+        public string CategoryName { get; set; }
+
+        //public virtual ICollection<Employees> Employees { get; set; }
+
+    }
+}
